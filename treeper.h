fd23687cc6c4d1e8df2c026bbbecc2989009cdae
iamShashi <Shashi.chandra101@gmail.com>
2020-03-30 02:57:35 +0530

    Development Of modules of each DSA section

--- treeper.h
+++ treeper.h
@@ -2,357 +2,488 @@
 #include<conio.h>
 #include<stdlib.h>
 
-//Heap OPerations
 
-//also AVL tree crud operations
-struct tree{
-	int data;
-	struct tree* left;
-	struct tree* right;
-};
 
-void printDLL(struct tree* node)
-{
-	while(node->right)
+	/*	A structure to create a tree. Its been assumed this the
+		most used structure of a node of a tree. Also tree structure
+		may vary from end to end.
+	*/
+	
+	struct tree{
+		int data;
+		struct tree* left;
+		struct tree* right;
+	};
+
+
+	/*	This Function prints a doubly linked list also called
+		as DLL inorder to call this function just pass the
+		address of the root node as an argument.
+	*/
+	
+	void printDLL(struct tree* node)
 	{
-		printf("\n%d",node->data);
-		node = node->right;
+		while(node->right)
+		{
+			printf("\n%d",node->data);
+			node = node->right;
+		}
 	}
-}
 
 
-void reverseDLL(struct tree* head, struct tree* &headSt){
-	struct tree* p = head;
-	struct tree* q = head->right;
-	while(q->right)
-	{
-		struct tree* a;
-//		a = p->left;
-//		q = p->right;
-		a = q;
-//		if(!a) p->right = null;
-		p->left = q;
-		headSt = q;
-		q->right  = p;
-		q->left  = NULL;
-		q = p;
-		a->right = q;
+	/* This function Reverses a DLL or also called as doubly
+		Linked List. In Order to call this function just pass
+		head of the DLL and an address where to store the new
+		Head after reversal.
+	*/
+	
+	void reverseDLL(struct tree* head, struct tree* &headSt){
+		struct tree* p = head;
+		struct tree* q = head->right;
+		while(q->right)
+		{
+			struct tree* a;
+	//		a = p->left;
+	//		q = p->right;
+			a = q;
+	//		if(!a) p->right = null;
+			p->left = q;
+			headSt = q;
+			q->right  = p;
+			q->left  = NULL;
+			q = p;
+			a->right = q;
+		}
 	}
-}
 
-//void isIsoMorphic(){
-//	
-//}
-//
-//void isMirror(){
-//	
-//}
-//
-//void diagonalTraversal(){
-//	
-//}
-//
-//
-//void deleteNodeBST(){
-//	
-//}
-//
-//
-//void dfsTraversal(){
-//	
-//}
-//
-//void bfsTraversal(){
-//	
-//}
 
-void binaryTreeToDLL(struct tree* node, struct tree* &head){
-	if(!node) return;
+	/*
+		This Function is meant to check whether a given tree
+		is balanced or not.In order to call this function
+		you need to pass the root node and a variables
+		address which will contain in binary whether the
+		given tree is balanced or not.
+	*/
 	
-	binaryTreeToDLL(node->left,head);
-	
-	struct tree* right = node->right;
-	
-	node->right = head;
-	if(head!=NULL)
+	int isHeightBalancedTree(struct tree* node, int* isBalanced)
 	{
-		head->left = node;
+		if(!node) return 0;
+		int LHeight = isHeightBalancedTree(node->left,isBalanced)+1;
+		int RHeight = isHeightBalancedTree(node->right,isBalanced)+1;
+		LHeight+1 == RHeight ? *isBalanced = 1 : LHeight == RHeight +1 ? *isBalanced = 1 : LHeight == RHeight ? *isBalanced = 1 : *isBalanced = 0;
+		return LHeight>=RHeight ? LHeight : RHeight;
+	}
+	
+	//void isIsoMorphic(){
+	//	
+	//}
+	//
+	//void isMirror(){
+	//	
+	//}
+	//
+	//void diagonalTraversal(){
+	//	
+	//}
+	//
+	//
+	//void deleteNodeBST(){
+	//	
+	//}
+	//
+	//
+	//void dfsTraversal(){
+	//	
+	//}
+	//
+	//void bfsTraversal(){
+	//	
+	//}
+
+	/*	This function is designed in order to create a Doubly Linked
+		List or a DLL from a binary tree.Things to note here are:
+		1. It is a simple Inorder traversal of a tree.
+		2. You will have to reverse the DLL to get the final real DLL.
+	*/
+	
+	void binaryTreeToDLL(struct tree* node, struct tree* &head){
+		if(!node) return;
 		
+		binaryTreeToDLL(node->left,head);
+		struct tree* right = node->right;
+		node->right = head;
+		if(head!=NULL)
+		{
+			head->left = node;
+		}
+		head = node;
+		binaryTreeToDLL(right,head);
 	}
-	
-	head = node;
-	
-	binaryTreeToDLL(right,head);
-}
 
 
-
-void smallest(struct tree* node,int* head)
-{
-	if(!node) return;
+	/*	This Function finds the smallest Value in a tree
+		It traverses each node and compares with the value
+		stored in each node with the value in the head
+		Pointer.
+	*/
 	
-	if(*head>=node->data)
-		*head = node->data;
+	void smallest(struct tree* node,int* head)
+	{
+		if(!node) return;
 		
-
-		smallest(node->left,head);
-		smallest(node->right,head);
-}
-
-
-void findfirstleft(struct tree* node,int *ft,int elem){
-	if(!node)
-		 	return;
+		if(*head>=node->data)
+			*head = node->data;
+			
 	
-	if(node->data>elem)
+			smallest(node->left,head);
+			smallest(node->right,head);
+	}
+
+
+	/*	This function helps in finding the first left of a
+		element in a binary Tree in order to call the same
+		pass the root node, an address pointer to an integer
+		and the elem.
+	*/
+	
+	void findfirstleft(struct tree* node,int *ft,int elem){
+		if(!node)
+			return;
+		
+		if(node->data>elem)
 		{
 			*ft = node->data;
 			findfirstleft(node->left,ft,elem);
 		}
 		else
 			findfirstleft(node->right,ft,elem);
-}
-
-
-void inorderSuccesor(struct tree* node,int elem,int* head,struct tree* root)
-{
-	if(!node)
-		return;
+	}
 	
-	if(node->data == elem)
+	/*	This function finds the first InOrder Successor of 
+		an element that is passed.To call this function
+		pass the root node, the element itself,a pointer
+		to an integer which will have the Successor at last
+		, and also the node.
+	*/
+	
+	void inorderSuccesor(struct tree* node,int elem,int* head,struct tree* root)
 	{
-		if(node->right)
+		if(!node)
+			return;
+		
+		if(node->data == elem)
 		{
-			int smal = 999;
-			smallest(node->right,&smal);
-			*head = smal;
-		}
-		else
-		{
-			int ftLeft = NULL;
-			findfirstleft(root,&ftLeft,elem);
-			*head = ftLeft;
-			if(ftLeft==NULL)
-				printf("This is the Last element of the tree in inorder successor %d",node->data);
-		}
-	}
-	if(node->data<elem)
-	{
-		inorderSuccesor(node->right,elem,head,root);	
-	}
-	else if(node->data>elem)
-	{
-		inorderSuccesor(node->left,elem,head,root);	
-	}		
-}
-
-void findBiggestNodeTree(struct tree* node,int *bgt){
-	if(!node) return;
-	
-	if(node->data>=*bgt)
-	{
-		*bgt = node->data;
-	}
-	findBiggestNodeTree(node->left,bgt);
-	findBiggestNodeTree(node->right,bgt);
-	
-}
-
-void firstRight(struct tree* node,int elem,int* trck)
-{
-	if(!node) return;
-	if(node->data<elem){
-		*trck = node->data;
-		firstRight(node->right,elem,trck);
-	}
-	else if(node->data>elem)
-	{
-		firstRight(node->left,elem,trck);
-	}
-}
-
-
-
-void inorderPredecessor(struct tree* node,int *head,struct tree* root,int elem){
-	if(!node) return;
-	
-	if(node->data==elem)
-	{
-		if(node->left)
-		{
-			int bgNt = -999;
-			findBiggestNodeTree(node->left,&bgNt);
-			*head = bgNt;
-		}
-		else
+			if(node->right)
 			{
-				int trckpath = NULL;
-				firstRight(root,node->data,&trckpath);
-				*head = trckpath;
-				if(trckpath==NULL)
-				{
-					printf("\n This is the LeftMost Element and Hence no Inorder Predecessor");
-				}
+				int smal = 999;
+				smallest(node->right,&smal);
+				*head = smal;
 			}
-	}
-	if(node->data>elem)
-	{
-		inorderPredecessor(node->left,head,root,elem);
-	}
-	else if(node->data<elem)
-	{
-		inorderPredecessor(node->right,head,root,elem);
-	}
-}
-
-struct tree* newNode(int data) 
-{ 
-     struct tree* node = (struct tree*) 
-                                  malloc(sizeof(struct tree)); 
-     node->data = data; 
-     node->left = NULL; 
-     node->right = NULL; 
-  
-     return(node); 
-} 
-
-
-
-void greatest(struct tree* node,int* head)
-{
-	if(!node) return;
-	
-	if(*head<=node->data)
-		*head = node->data;
-		
-
-		greatest(node->left,head);
-		greatest(node->right,head);
-}
-
-void preorder(struct tree* node)
-{
-	if(node==NULL)
-		return;
-	
-	printf("\n%d",node->data);
-	preorder(node->left);
-	preorder(node->right);
-}
-
-void postorder(struct tree* node)
-{
-	if(node==NULL)
-		return;
-	
-	
-	postorder(node->left);
-	postorder(node->right);
-	printf("\n%d",node->data);
-}
-
-//struct tree* findrightPos(struct tree* node,int datain){
-//
-//	if(node==NULL)
-//		return NULL;
-//	if(node->data>datain)
-//		{
-//			printf("\nadding to left of it upar %d",node->data);
-//			findrightPos(node->left,datain);
-//			if(rightpos==NULL)
-//			rightpos = node;
-//		}
-//	else if(node->data<datain)
-//		{
-//			printf("\nadding to right of it upar %d",node->data);
-//			findrightPos(node->right,datain);
-//			if(rightpos==NULL)
-//			rightpos = node;
-//		}
-//	return rightpos;
-//}
-
-void insertnode(struct tree* node,int datain)
-{
-	if(node==NULL)
-		return ;
-	
-	if(node->data>=datain)
-	{
-		if(node->left==NULL)
-		{
-			node->left=newNode(datain);
-//			node->left->data = datain;
-printf("\nadding to left of it %d",node->data);
+			else
+			{
+				int ftLeft = NULL;
+				findfirstleft(root,&ftLeft,elem);
+				*head = ftLeft;
+				if(ftLeft==NULL)
+					printf("This is the Last element of the tree in inorder successor %d",node->data);
+			}
 		}
-		else
-			insertnode(node->left,datain);
-	}
-	else
-	{
-		if(node->right==NULL)
+		if(node->data<elem)
 		{
-			node->right=newNode(datain);
-			printf("\nadding to right of it %d",node->data);
-//			node->right->data= datain;
+			inorderSuccesor(node->right,elem,head,root);	
 		}
-		else
-			insertnode(node->right,datain);
+		else if(node->data>elem)
+		{
+			inorderSuccesor(node->left,elem,head,root);	
+		}		
 	}
 	
-}
-
-void inorder(struct tree* node){
-	if(node==NULL)
-	return;
+	/*	This Function finds the biggest Node of a given
+		tree. In order to call this function pass the
+		root node, the pointer to the integer where
+		the data will be stored at last.
+	*/
 	
-		inorder(node->left);
-	
-	printf("\n%d ->",node->data);
-	
-		inorder(node->right);
-	
-	
-}
-
-struct tree* createTree(int count){
-	struct tree* root = NULL;
-	while(count)
-	{
-		int val;
-		struct tree* newnode = NULL;
+	void findBiggestNodeTree(struct tree* node,int *bgt){
+		if(!node) return;
 		
-		printf("\nEnter the node value");
-		scanf("%d",&val);
-		newnode = newNode(val);
-		if(root==NULL)
+		if(node->data>=*bgt)
 		{
-			root = newnode;
-			printf("\nPrinting inorder");
-			inorder(root);
+			*bgt = node->data;
+		}
+		findBiggestNodeTree(node->left,bgt);
+		findBiggestNodeTree(node->right,bgt);
+		
+	}
+	
+	/*	This Function finds the first Right element of a given
+		node in a tree. In order to call this function pass
+		the root node , the element whose right element is
+		to be found, the pointer to integer which will have
+		the node value.
+	*/
+	
+	void firstRight(struct tree* node,int elem,int* trck)
+	{
+		if(!node) return;
+		if(node->data<elem){
+			*trck = node->data;
+			firstRight(node->right,elem,trck);
+		}
+		else if(node->data>elem)
+		{
+			firstRight(node->left,elem,trck);
+		}
+	}
+	
+
+	/*	This function finds the first InOrder Predecessor of 
+		an element that is passed.To call this function
+		pass the root node, the element itself,a pointer
+		to an integer which will have the Predecessor at last
+		, and also the node.
+	*/
+	
+	void inorderPredecessor(struct tree* node,int *head,struct tree* root,int elem){
+		if(!node) return;
+		
+		if(node->data==elem)
+		{
+			if(node->left)
+			{
+				int bgNt = -999;
+				findBiggestNodeTree(node->left,&bgNt);
+				*head = bgNt;
+			}
+			else
+				{
+					int trckpath = NULL;
+					firstRight(root,node->data,&trckpath);
+					*head = trckpath;
+					if(trckpath==NULL)
+					{
+						printf("\n This is the LeftMost Element and Hence no Inorder Predecessor");
+					}
+				}
+		}
+		if(node->data>elem)
+		{
+			inorderPredecessor(node->left,head,root,elem);
+		}
+		else if(node->data<elem)
+		{
+			inorderPredecessor(node->right,head,root,elem);
+		}
+	}
+	
+	/*	This function helps in creating a new node and
+		also allocating a space for the tree at runtime
+		call this function with data value and it will
+		return the address of the node.
+	*/
+	
+	struct tree* newNode(int data) 
+	{ 
+	     struct tree* node = (struct tree*) 
+	                                  malloc(sizeof(struct tree)); 
+	     node->data = data; 
+	     node->left = NULL; 
+	     node->right = NULL; 
+	  
+	     return(node); 
+	} 
+	
+	/*	This function helps in finding the greatest
+		node value in a given tree. In order to call
+		this function pass the root node and also
+		the pointer to integer where value will be
+		kept.
+	*/
+	
+	void greatest(struct tree* node,int* head)
+	{
+		if(!node) return;
+		
+		if(*head<=node->data)
+			*head = node->data;
 			
+	
+			greatest(node->left,head);
+			greatest(node->right,head);
+	}
+	
+	/*	This function helps in printing the PREorder
+		form of a tree in order to call the same
+		pass the root node of the tree.
+	*/
+	void preorder(struct tree* node)
+	{
+		if(node==NULL)
+			return;
+		
+		printf("\n%d",node->data);
+		preorder(node->left);
+		preorder(node->right);
+	}
+	
+	/*	This function helps in printing the POST order
+		form of a tree in order to call the same
+		pass the root node of the tree.
+	*/
+	
+	void postorder(struct tree* node)
+	{
+		if(node==NULL)
+			return;
+		
+		
+		postorder(node->left);
+		postorder(node->right);
+		printf("\n%d",node->data);
+	}
+	
+	//struct tree* findrightPos(struct tree* node,int datain){
+	//
+	//	if(node==NULL)
+	//		return NULL;
+	//	if(node->data>datain)
+	//		{
+	//			printf("\nadding to left of it upar %d",node->data);
+	//			findrightPos(node->left,datain);
+	//			if(rightpos==NULL)
+	//			rightpos = node;
+	//		}
+	//	else if(node->data<datain)
+	//		{
+	//			printf("\nadding to right of it upar %d",node->data);
+	//			findrightPos(node->right,datain);
+	//			if(rightpos==NULL)
+	//			rightpos = node;
+	//		}
+	//	return rightpos;
+	//}
+	
+	/*	This function helps in inserting a new node in a
+		BST it traverses the tree and finds the right spot
+		for the same. In order to call the function
+		pass the root node and the value to be inserted.
+	*/
+	
+	void insertnode(struct tree* node,int datain)
+	{
+		if(node==NULL)
+			return ;
+		
+		if(node->data>=datain)
+		{
+			if(node->left==NULL)
+			{
+				node->left=newNode(datain);
+	//			node->left->data = datain;
+	printf("\nadding to left of it %d",node->data);
+			}
+			else
+				insertnode(node->left,datain);
 		}
 		else
 		{
-			struct tree* rightpos = NULL;
-			insertnode(root,val);
-//			if(nodeplace->data<val)
-//			{
-//				printf("\nadding to right of it %d",nodeplace->data);
-//				nodeplace->right = newnode;
-//			}
-//			else if(nodeplace->data>val)
-//			{
-//				printf("\nadding to left of it %d",nodeplace->data);
-//				nodeplace->left = newnode;
-//			}
+			if(node->right==NULL)
+			{
+				node->right=newNode(datain);
+				printf("\nadding to right of it %d",node->data);
+	//			node->right->data= datain;
+			}
+			else
+				insertnode(node->right,datain);
 		}
-		count--;
+		
 	}
 	
-	return root;
-}
+	/*	This function helps in printing the INorder
+		form of a tree in order to call the same
+		pass the root node of the tree.
+	*/
 	
+	void inorder(struct tree* node){
+		if(node==NULL)
+		return;
+		
+			inorder(node->left);
+		
+		printf("\n%d ->",node->data);
+		
+			inorder(node->right);
+		
+		
+	}
 	
+	/*	This function helps in creating a binary
+		search tree in order to call the same
+		pass count of the nodes to be kept in the
+		tree. It asks for the value next to be 
+		inserted in the tree after inserting one
+		untill all the values are inserted.
+		It returns the root node of the tree
+		Created.
+	*/
+	
+	struct tree* createTree(int count){
+		struct tree* root = NULL;
+		while(count)
+		{
+			int val;
+			struct tree* newnode = NULL;
+			
+			printf("\nEnter the node value");
+			scanf("%d",&val);
+			newnode = newNode(val);
+			if(root==NULL)
+			{
+				root = newnode;
+				printf("\nPrinting inorder");
+				inorder(root);
+				
+			}
+			else
+			{
+				struct tree* rightpos = NULL;
+				insertnode(root,val);
+	//			if(nodeplace->data<val)
+	//			{
+	//				printf("\nadding to right of it %d",nodeplace->data);
+	//				nodeplace->right = newnode;
+	//			}
+	//			else if(nodeplace->data>val)
+	//			{
+	//				printf("\nadding to left of it %d",nodeplace->data);
+	//				nodeplace->left = newnode;
+	//			}
+			}
+			count--;
+		}
+		
+		return root;
+	}
+	
+	/*	This function is to serialize a binary tree.
+		Serialize as the name suggests it will convert
+		and save the whole tree in an array. In order
+		to call the same pass the root node , the
+		base address of array of pointers that will
+		keep the pointer to a node and index which will
+		help in traversing the array during recursion.
+		Initially pass 0 as index.
+	*/
 
-
+	void serializetheTree(struct tree* node, struct tree** &arr,int index){
+		if(!node)
+		{
+			arr[index] = NULL;
+			return;
+		}
+		if(node)
+		arr[index] = node;
+		serializetheTree(node->left, arr,index*2+1);
+		serializetheTree(node->right,arr,index*2+2);
+		
+	}
--- utilityFunction.cpp
+++ utilityFunction.cpp
@@ -3,26 +3,42 @@
 #include "treeper.h"
 
 
+void initiateTreeModule(char username[50])
+{	
 
-
-
-
-
-
-
-int main(){
+	printf("\nHi %s In Trees we will Cover the following Questions.\n\n",username);
+	printf("\n1. Creating a BST.");
+	printf("\n2. Printing a INORDER Traversal");
+	printf("\n3. Printing a POSTORDER Traversal");
+	printf("\n4. Printing a PREORDER Traversal");
+	printf("\n5. Checking For Balanced BST");
+	printf("\n6. Converting a Binary Tree to Doubly Linked List");
+	printf("\n7. Finding a smallest Element of a BST");
+	printf("\n8. Finding the First Left of a node in BST");
+	printf("\n9. Finding the First Right of a node in a BST");
+	printf("\n10. Finding the Inorder Successor of a given node");
+	printf("\n11. Finding the Biggest Node in a BST");
+	printf("\n12. Finding the Inorder Predecessor of a given node");
+	printf("\n13. Finding the Right Place to Insert a given node In BST");
+	printf("\n14. Converting the tree into an Array Representation of the same.\n\n");
+	
+	printf("\nSo Please select one of these by entering the number");
+	printf("\nIn order to continue OR");
+	printf("Enter '-1' to go back ");
+	printf("to Previous section");
+	int treeChoice;
+	scanf("%d",&treeChoice);
 	int count;
 	printf("Enter No. of nodes in the tree");
 	scanf("%d",&count);
 	struct tree* root = NULL;
 	root = createTree(count);
-	
-//	struct tree *root  = newNode(1); 
-//     root->left             = newNode(2); 
-//     root->right           = newNode(3); 
-//     root->left->left     = newNode(4); 
-//     root->left->right   = newNode(5);
-	
+			
+
+/*
+	//	These functions print Inorder/Preorder/Postorder of a tree
+	//	In Order to Run them just Uncomment the following Lines.
+	//
 	printf("\nPrinting inorder");
 	inorder(root);
 	printf("\nPrinting preorder");
@@ -31,16 +47,21 @@ int main(){
 	postorder(root);
 	
 	
-	
-//	smallest = 999;
-	
+	//	This function finds the smallest element of the tree given.
+	//
 	int smallst = 999;
 	smallest(root,&smallst);
 	printf("\n the smallest element is %d",smallst);
 	
+	//  This function finds the largest element of the tree given.
+	//
 	int grtst = -999;
 	greatest(root,&grtst);
 	printf("\n the greatest element is %d",grtst);
+	
+	//	This function helps in finding the InOrder Successor of a given
+	//	node.
+	//
 	int wil=1;
 	while(wil==1)
 	{
@@ -53,6 +74,10 @@ int main(){
 		printf("\nEnter the will");
 		scanf("%d",&wil);
 	}
+	
+	//	This function helps in finding the InOrder Predecessor of a given
+	//	node.
+	//
 	wil = 1;
 	while(wil==1)
 	{
@@ -64,5 +89,76 @@ int main(){
 		printf("\nthe node Predeccessor is %d",valtoStore);
 		printf("\nEnter the will");
 		scanf("%d",&wil);
+	}
+*/
+	//	int isBalanced = -1;
+	//	isHeightBalancedTree(root, &isBalanced);
+	//	if(isBalanced)
+	//		printf("\nTree seems to be Balanced");
+	//	else
+	//		printf("\nTree seems not  to be Balanced");
+	
+	struct tree **serializedArray = (struct tree**)malloc(sizeof(struct tree *)*2*count);
+	for(int i=0;i<=2*count;i++)
+	{
+		serializedArray[i] = NULL;
+	}
+	serializetheTree(root,serializedArray,0);
+	printf("\nPrinting the serialized array:");
+	for(int i=0;i<=2*count;i++)
+	{
+		if(serializedArray[i])
+		{
+			if(i==0)
+				printf("%d",serializedArray[i]->data);
+			else	
+				printf("-> %d",serializedArray[i]->data);
+		}
+		else if(!serializedArray[i])
+			printf("-> NULL");
+	}
+}
+
+int main(){
+	printf("Welcome to C Data Structures and Alogrithm Self Course...\n\n");
+	printf("Please wait while all the Modules are Loaded..\n");
+	char username[50];
+	printf("Please remind what we should call you?\n");
+	scanf("%[^\n]%*c",username);
+	printf("\nThe whole Module consists of the following\n");
+	printf("\n1. ARRAYS.");
+	printf("\n2. Linked List.");
+	printf("\n3. Stacks.");
+	printf("\n4. Queues.");
+	printf("\n5. Trees.");
+	printf("\n6. Heaps.\n\n");
+	
+	int userChoiceModule;
+	printf("\nHi %s Enter your choice of Module\n\n",username);
+	scanf("%d",&userChoiceModule);
+	switch(userChoiceModule)
+	{
+		case 1:
+			printf("AW snap!! this section is under construction.\n");
+			break;
+		case 2:
+			printf("AW snap!! this section is under construction\n");
+			break;
+		case 3:
+			printf("AW snap!! sorry this section is under construction\n");
+			break;
+		case 4:
+			printf("AW snap!! sorry this section is under construction\n");
+			break;
+		case 5:
+			printf("KABOOOM! Get Ready for this section as it is going to be one of the Most\n");
+			printf("interesting things for you to learn.\n\n");
+			printf("So what you doing ?! Buckle up and Get something to solve it somewhere.");
+			printf("\n\n Initiating Tree Module...");
+			initiateTreeModule(username);
+			break;
+		case 6:
+			printf("AW snap!! sorry this section is under construction\n");
+			break;
 	}
 }
